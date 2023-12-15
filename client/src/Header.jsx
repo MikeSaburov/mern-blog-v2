1f231fc7c58ad1353197f88f38da40dd2f414afb
Mikhail Saburov <saburov1985@gmail.com>
2023-12-15 22:59:32 +1100

    feature: Добавил сверстанный код (client)

---
+++ client/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <header>
+      <Link className="logo" to="/">
+        MyBlog
+      </Link>
+      <nav>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+    </header>
+  );
+};
